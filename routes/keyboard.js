3ca8113baeb81630cb82462be697f038dc5a6430
monica <monica.kwon@kakaocorp.com>
2018-11-07 16:22:35 +0900

    first commit

---
+++ routes/keyboard.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* 처음 들어왔을 때 */
+router.get('/', function(req, res, next) {
+const menu = {
+type: 'buttons',
+buttons: ["노래 추천해줘.", "네 노래로 추천해줘."]
+};
+res.set({
+'content-type': 'application/json'
+}).send(JSON.stringify(menu));
+});
+
+module.exports = router;
---
+++ routes/message.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var router = express.Router();
+
+/* 사용자의 답장이 들어왔을 때 */
+router.post("/", function(req, res, next) {
+  const object = {
+    // 1
+    user_key: req.body.user_key, // 메시지를 발송한 user을 식별할 수 있는 key
+    type: req.body.type, // user가 보낸 message의 형태. text , photo로 이루어짐
+    content: req.body.content // user가 보낸 메시지 내용.
+  };
+  const menu = {
+    //2
+    type: "buttons",
+    buttons: ["안녕", "처음만난 날은?"]
+  };
+  var res_object; //3
+  if (object.type == "text") {
+    if (object.content == "안녕") {
+      //4
+      res_object = {
+        message: {
+          text: "안녕 뽀뽀좀 해줘봐!"
+        },
+        keyboard: menu
+      };
+    } else if (object.content == "처음만난 날은?") {
+      //5
+      res_object = {
+        message: {
+          text: "2018년 4월 27일이야!"
+        },
+        keyboard: menu
+      };
+    } else {
+      res_object = {
+        message: {
+          text: object.content
+        },
+        keyboard: menu
+      };
+    }
+  }
+  res
+    .set({
+      //6
+      "content-type": "application/json"
+    })
+    .send(JSON.stringify(res_object));
+});
+
+module.exports = router;
